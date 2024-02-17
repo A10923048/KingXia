a2c5de50c55b748d0101641f7957b7617ae04afc
A10923048 <amy10010132@gmail.com>
2024-02-17 17:30:26 +0800

    建立表單

--- tickets/js/ticketsPage_copy.js
+++ tickets/js/ticketsPage_copy.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
         $('.messages-section').removeClass('show');
     });
 
-    // login
+    // 放置"signup"屬性表open哪個頁面
     $('#login').click(function() {
         let parent = $(this).parent().parent();
         if (!parent.hasClass('slide-up')) {
@@ -98,6 +98,44 @@ $(document).ready(function() {
         $(target).fadeIn(600);
         
     });
+
+
+    //註冊帳號"送出"按鈕
+    $("#sendRegistered").click(function() { // 當送出按鈕被點擊時
+    
+        $('.tab-content form').submit(function(event) {
+               
+            // 阻止表單的提交行為
+            event.preventDefault();
+            
+            let inputValues = []; // 用於存儲 input 值的列表
+
+            // 對於每個 input 元素
+            $(".tab-content input").each(function() {
+                var value = $(this).val(); // 獲取 input 的值
+
+                if (value.trim() !== "") { // 如果值不為空（去除頭尾空格後）
+                    inputValues.push(value); // 添加到列表中
+            
+                }
+            });
+
+
+
+            console.log(JSON.stringify(inputValues, null, 2));
+
+            // if (inputValues.length > 0) { // 如果列表中有值
+            //     console.log("Input values are not empty:");
+            //     console.log(inputValues); // 輸出列表中的值
+            // } else {
+            //     console.log("Input values are empty.");
+            // }
+            
+        });
+
+    });
+    
+
 
 
 });
